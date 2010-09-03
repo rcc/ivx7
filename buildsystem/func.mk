e004d257bb49abb1fbe340556cb5be0823f2fa11
Robert Curtis <robert.caston@gmail.com>
2010-09-03 13:52:35 -0700

    making OUTPUTINFO truncate long lines

--- buildsystem/func.mk
+++ buildsystem/func.mk
@@ -11,6 +11,16 @@ RMTRAILINGSLASH = $(patsubst %/,%,$1)
 USCORESUB = $(shell echo $1 | sed 'y; -./;____;')
 
 OUTPUTINFO = @echo "[$1],$2" | \
-	     awk 'BEGIN {FS = "[ \t]*,[ \t]*";} {printf("%-15s %s\n",$$1,$$2);}'
+		awk 'BEGIN { \
+			FS = "[ \t]*,[ \t]*"; \
+			L = 60; \
+		} \
+		{ \
+			s = $$2; \
+			if(length(s) > L) { \
+				s = "... " substr(s,length(s)-L+1,length(s)); \
+			} \
+			printf("%-15s %s\n",$$1,s); \
+		}'
 
 PRINTLIST = @echo $1 | awk '{for(ii=1;ii<=NF;ii++){print "$2" $$ii;}}'
