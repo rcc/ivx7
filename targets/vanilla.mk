ba763945112e7e76e098e33090e4288ac5e896a4
Robert Curtis <robert.caston@gmail.com>
2011-08-19 16:29:50 -0700

    integrating snow branch back as a vanilla config for now

--- targets/vanilla.mk
+++ targets/vanilla.mk
@@ -11,6 +11,12 @@ SOURCES :=	src/main.c \
 		lib/cmds_shell.c \
 		src/cmds/version.c \
 
+SNOW_SOURCES +=	lib/threadpool.c \
+		lib/net/util.c \
+		lib/net/pkv.c \
+		lib/net/connection.c
+		#lib/net/listen.c
+
 # Libraries
 LIBRARIES := readline
 
@@ -24,7 +30,7 @@ CPPFLAGS += -DBUILD_DATE='"$(shell date)"'
 # Options
 
 # Configurations (the first one is the default)
-CONFIGS := debug release
+CONFIGS := debug release snow
 
 # Configuration Specific Options
 RELEASE_OPTIONS := CONFIG_RELEASE
@@ -33,9 +39,15 @@ RELEASE_OPTIONS += MAX_LOGLEVEL=3 DEFAULT_LOGLEVEL=2
 DEBUG_OPTIONS := CONFIG_DEBUG
 DEBUG_OPTIONS += MAX_LOGLEVEL=5 DEFAULT_LOGLEVEL=4
 
+SNOW_OPTIONS += MAX_LOGLEVEL=5 DEFAULT_LOGLEVEL=4
+SNOW_OPTIONS += THREADPOOLTESTCMD NETCONNECTIONTESTCMD
+
 # Configuration Specific Flags
 RELEASE_CFLAGS := -O2
 RELEASE_CXXFLAGS := $(RELEASE_CFLAGS)
 
 DEBUG_CFLAGS := -O0 -g
 DEBUG_CXXFLAGS := $(DEBUG_CFLAGS)
+
+SNOW_CFLAGS := $(DEBUG_CFLAGS)
+SNOW_CXXFLAGS := $(DEBUG_CXXFLAGS)
