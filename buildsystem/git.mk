a8facd9a7c11d5d1c0f0de050c90acfe169f1163
Robert Curtis <robert.caston@gmail.com>
2011-09-15 09:27:44 -0700

    cleanup

--- buildsystem/git.mk
+++ buildsystem/git.mk
@@ -1,4 +1,5 @@
 SCMVERSION := $(shell git describe --tags --dirty=+ 2>/dev/null || \
 		git rev-parse --short=16 HEAD 2>/dev/null || \
-	    	echo "UNKNOWN")
-SCMBRANCH := $(shell (git branch 2> /dev/null || echo '* UNKNOWN') | awk '/^\* / {print $$2;}')
+		echo "UNKNOWN")
+SCMBRANCH := $(shell (git branch 2> /dev/null || echo '* UNKNOWN') | \
+		awk '/^\* / {print $$2;}')
